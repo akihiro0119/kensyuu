22e2b998fa487c449f46ac24e5d91081be629d7a
akihiro0119 <aono1x19@gmail.com>
2021-02-05 11:30:52 +0900

    サンプルコードの電卓、自分自身の電卓

---
+++ Calc.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Calc {
+
+public static void main(String[] args){
+  Scanner s = new Scanner(System.in);
+
+  System.out.println("3桁の数字を入力してください");
+
+    int a = s.nextInt();
+  
+    System.out.println("2桁の数字を入力してください");
+
+    int b = s.nextInt();
+
+    s.close();
+
+    int total = CalcLogic.plus(a,b);
+    int delta = CalcLogic.minus(a,b);
+    int call = CalcLogic.call(a,b);
+    int waru = CalcLogic.waru(a,b);
+    int amari = CalcLogic.waru(a,b);
+
+    System.out.println("足したら" + total + ",引いたら" + delta + ",かけたら" + call + ",割ったら" + waru +"あまりは" + amari);
+    }
+}
---
+++ CalcLogic.java
@@ -0,0 +1,20 @@
+public class CalcLogic {
+  public static int plus(int a, int b){
+    return(a + b);
+  }
+  public static int minus(int a, int b){
+    return(a - b);
+  }
+
+  public static int call(int a, int b){
+    return(a * b);
+  }
+
+  public static int waru(int a, int b){
+    return(a / b);
+  }
+
+  public static int amari(int a, int b){
+    return(a % b);
+  }
+}
